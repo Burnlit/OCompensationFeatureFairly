156153094b5ee809bbaa14aa2d21572fc6b53868
Lawrence Caballes <licaballes@gmail.com>
2023-10-10 17:54:01 -0700

    Update CompensationTypeLabels.cs

--- Domains/CompensationTypeLabels/CompensationTypeLabels.cs
+++ Domains/CompensationTypeLabels/CompensationTypeLabels.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Domain.CompensationTypeLabels
+namespace Models.Domain.CompensationTypeLabels
 {
     public class CompensationTypeLabel
     {
